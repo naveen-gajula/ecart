9dfb2daae9562de7c0cdb443532336dbd7e1c4ca
naveen-gajula <naveen.gajula22@gmail.com>
2022-01-18 01:22:38 +0530

    initial commit

---
+++ src/main/java/com/example/ecommerce/app/EcommerceBillingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce/ecommercebilling/EcommerceBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.ecommercebilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
